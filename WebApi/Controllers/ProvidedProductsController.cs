f032090b4cbc70238f0e76a22dfb14f5b20d6777
Jetug <ksergeev01@gmail.com>
2022-06-02 11:29:05 +0300

    crud 6

---
+++ WebApi/Controllers/ProvidedProductsController.cs
@@ -0,0 +1,55 @@
+﻿using EntityFramework.Data.Tables;
+using EntityFramework.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProvidedProductsController : ControllerBase
+    {
+        private readonly MyDbContext context;
+
+        public ProvidedProductsController(MyDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        [Route("all")]
+        public IActionResult GetAllGadtets()
+        {
+            return Ok(context.GetTableByType(new ProvidedProduct()).ToList());
+        }
+
+        [HttpPost]
+        [Route("add")]
+        public IActionResult CreateProvidedProduct(ProvidedProduct model)
+        {
+            context.AddNSave(model);
+            return Ok(model.Id);
+        }
+
+        [HttpPut]
+        [Route("update")]
+        public IActionResult UpdateProvidedProduct(ProvidedProduct model)
+        {
+            context.UpdateNSave(model);
+            return NoContent();
+        }
+
+        [HttpDelete]
+        [Route("delete/{id}")]
+        public IActionResult DeleteProvidedProduct(int id)
+        {
+            var model = context.ProvidedProducts.Find(id);
+            if (model == null)
+                return NotFound();
+
+            context.DeleteNSave(model);
+            return NoContent();
+        }
+    }
+}
---
+++ WebApi/Controllers/SalesDataController.cs
@@ -0,0 +1,55 @@
+﻿using EntityFramework.Data.Tables;
+using EntityFramework.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SalesDataController : ControllerBase
+    {
+        private readonly MyDbContext context;
+
+        public SalesDataController(MyDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        [Route("all")]
+        public IActionResult GetAllGadtets()
+        {
+            return Ok(context.GetTableByType(new SaleData()).ToList());
+        }
+
+        [HttpPost]
+        [Route("add")]
+        public IActionResult CreateSaleData(SaleData model)
+        {
+            context.AddNSave(model);
+            return Ok(model.Id);
+        }
+
+        [HttpPut]
+        [Route("update")]
+        public IActionResult UpdateSaleData(SaleData model)
+        {
+            context.UpdateNSave(model);
+            return NoContent();
+        }
+
+        [HttpDelete]
+        [Route("delete/{id}")]
+        public IActionResult DeleteSaleData(int id)
+        {
+            var model = context.SalesData.Find(id);
+            if (model == null)
+                return NotFound();
+
+            context.DeleteNSave(model);
+            return NoContent();
+        }
+    }
+}

02a7f905060378b2af21fa910bdec1f2471feb0c
Jetug <ksergeev01@gmail.com>
2022-06-02 12:44:40 +0300

    Основной функционал

--- WebApi/Controllers/MainController.cs
+++ WebApi/Controllers/MainController.cs
@@ -13,21 +13,12 @@ namespace WebApi.Controllers
     [Route("[controller]")]
     public class MainController : ControllerBase
     {
-        //private readonly ILogger<MyController> _logger;
-        private readonly MyDbContext myDbContext;
         private readonly Operations operations;
 
         public MainController(MyDbContext dbContext)
         {
-            myDbContext = dbContext;
             operations = new Operations(dbContext);
-
         }
-
-        //public MyController(ILogger<MyController> logger)
-        //{
-        //    _logger = logger;
-        //}
 
         [HttpPost]
         [Route("Sale")]
